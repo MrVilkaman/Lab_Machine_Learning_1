1f217dc5cb044b553997ae3084421912189c5f15
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-10-24 23:10:15 +0300

    Added random train set

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,16 +1,40 @@
-import classification.*
+import classification.KMeans
+import classification.СlassificationCos
+import classification.СlassificationEuclid
+import classification.СlassificationTanimoto
 import dp.DataProviders
 import graph.DrawGraph
+import model.BaggageModel
 import utils.LabUtils
 import utils.TableUtils
+import java.util.*
 import javax.swing.SwingUtilities
 
 /**
  * Created by Zahar on 22.10.16.
  */
 
+val needRandom: Boolean = false
+
 fun main(args: Array<String>) {
-	val items = DataProviders.trainingSet
+
+
+	val items: ArrayList<BaggageModel> = if (needRandom) {
+		val arrayList = ArrayList<BaggageModel>()
+		val random = Random()
+		for (i in 0..9) {
+			val ds = ge(random)
+			val dm = ge(random)
+			val value = (3 * ds + 4 * dm) <= 30
+
+			val cla = if (value) 1 else 0
+			arrayList.add(BaggageModel(ds, dm, cla, i))
+		}
+		arrayList
+	} else {
+		DataProviders.trainingSet
+	}
+
 	val controlSet = DataProviders.controlSet
 
 //	println("Исходные денные:")
@@ -21,9 +45,9 @@ fun main(args: Array<String>) {
 
 	println()
 	println("Классификация сравнение с эталоном:")
-	val arrayList = listOf(СlassificationCos(), СlassificationEuclid(), СlassificationTanimoto())
-	TableUtils.printTable("Обучающая выборка", items, arrayList, centerPos)
-	TableUtils.printTable("Контрольная выборка", controlSet, arrayList, centerPos)
+	val arra = listOf(СlassificationCos(), СlassificationEuclid(), СlassificationTanimoto())
+	TableUtils.printTable("Обучающая выборка", items, arra, centerPos)
+	TableUtils.printTable("Контрольная выборка", controlSet, arra, centerPos)
 
 	println("Центры кластеров:")
 	centerPos.forEachIndexed { i, simplePoint ->
@@ -34,7 +58,7 @@ fun main(args: Array<String>) {
 
 	println()
 	println("Классификация K-means:")
-	arrayList.forEach {
+	arra.forEach {
 		KMeans(it).classifyKNN(items, controlSet, 3)
 	}
 
@@ -44,6 +68,13 @@ fun main(args: Array<String>) {
 
 
 }
-	fun Double.format(digits: Int) = java.lang.String.format("%.${digits}f", this)
+
+private fun ge(random: Random): Double {
+	val nextInt = random.nextInt(99).toDouble()
+	return nextInt / 10
+}
+
+
+fun Double.format(digits: Int) = java.lang.String.format("%.${digits}f", this)
 
 
